fcd7a3b277e474710d0afe6fa3ca8611a7dbf03b
Matej Marusak <mmarusak@redhat.com>
2018-12-15 13:12:44 +0100

    React on click
    Signed-off-by: Matej Marusak <mmarusak@redhat.com>

--- quarto/src/index.js
+++ quarto/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Board extends React.Component {
     renderRow(i){
         return [
-            React.createElement('button', {key: i + '1', className:'field'}),
-            React.createElement('button', {key: i + '2', className:'field'}),
-            React.createElement('button', {key: i + '3', className:'field'}),
-            React.createElement('button', {key: i + '4', className:'field'}),
+            <button className={this.props.value[i*4+0]} onClick={() => this.props.onClick(i,0)} />,
+            <button className={this.props.value[i*4+1]} onClick={() => this.props.onClick(i,1)} />,
+            <button className={this.props.value[i*4+2]} onClick={() => this.props.onClick(i,2)} />,
+            <button className={this.props.value[i*4+3]} onClick={() => this.props.onClick(i,3)} />,
         ]
     }
 
@@ -16,7 +17,7 @@ class Board extends React.Component {
         return(
             React.createElement(
                 'div',
-                {},
+                {className: 'col-md-6'},
                 React.createElement(
                     'div',
                     {className:'board-row'},
@@ -43,9 +44,35 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
-    return <Board />;
-  };
+    constructor(props){
+        super(props);
+        this.state = {
+          gameSquares: Array(16).fill('field'),
+          stackSquares: Array(16).fill('field'),
+        };
+    }
+
+    handleGameBoard = (x,y) => {
+    };
+
+    handlePiecesBoard = (x,y) => {
+        const squares = this.state.stackSquares.slice();
+        squares[x*4+y] = 'field selected';
+        this.setState({
+            stackSquares: squares,
+        });
+    };
+
+    render() {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <Board onClick={this.handleGameBoard} value={this.state.gameSquares}/>
+                    <Board onClick={this.handlePiecesBoard} value={this.state.stackSquares}/>
+                </div>
+            </div>
+        )
+    };
 }
 
 ReactDOM.render(
